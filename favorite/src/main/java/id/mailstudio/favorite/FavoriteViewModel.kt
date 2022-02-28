7887231245d2e757a9b852ba7b38bd19e4e6c7d7
isMail <65799702+ismail1229@users.noreply.github.com>
2022-02-28 18:15:47 +0700

    add linter setup

--- favorite/src/main/java/id/mailstudio/favorite/FavoriteViewModel.kt
+++ favorite/src/main/java/id/mailstudio/favorite/FavoriteViewModel.kt
@@ -4,7 +4,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
 import id.mailstudio.core.domain.usecase.FoodUseCase
 
-class FavoriteViewModel(private val foodUseCase: FoodUseCase) : ViewModel() {
+class FavoriteViewModel(foodUseCase: FoodUseCase) : ViewModel() {
 
     val favorite = foodUseCase.getFavoriteFood().asLiveData()
 }
